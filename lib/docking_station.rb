7902470ccd201d8cf4ebf58fd32ba08bacf40570
RachaelMahon <rachmahon@gmail.com>
2016-09-28 12:26:03 +0100

    Slaying it

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,16 @@
 class DockingStation
   def release_bike
+    bike = Bike.new
   end
 end
+
+
+class Bike
+ def is_working?
+   true
+ end
+end
+
+
 
 #check to commit
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,12 @@
 require 'docking_station'
 
+
 describe DockingStation do
     it { is_expected.to respond_to :release_bike}
+
+
+
+describe Bike do
+    it { is_expected.to respond_to :is_working?}
+end
 end
