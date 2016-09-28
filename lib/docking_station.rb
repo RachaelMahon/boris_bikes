5d36018a84f348861a16383ded85bf7646f9480e
Frankie Bell <fbell.pro@gmail.com>
2016-09-28 15:00:14 +0100

    added in dock bike and has bike - also created instance for Bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,16 +1,56 @@
 class DockingStation
-  def release_bike
-    bike = Bike.new
+
+  attr_reader :station
+  def initialize()
+    @station = station
   end
+
+
+
+  def release_bike
+  end
+
+  def dock_bike
+  end
+
+  def return_bike
+  end
+
+  def has_bike?
+    @station.empty?
+  end
+
 end
 
 
 class Bike
+
+  attr_reader :bike
+  def initialize()
+    @bike = bike
+  end
+
  def is_working?
    true
  end
+
 end
 
+=begin
 
+class Customer
+  def return
+    if DockingStation.empty?
+      nil
+    elsif !DockingStation.empty?
+
+    end
+  end
+
+end
+
+class
+
+=end
 
 #check to commit
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,10 +3,11 @@ require 'docking_station'
 
 describe DockingStation do
     it { is_expected.to respond_to :release_bike}
-
-
+    it { is_expected.to respond_to :dock_bike}
+    it { is_expected.to respond_to :has_bike?}
 
 describe Bike do
     it { is_expected.to respond_to :is_working?}
+
 end
 end
